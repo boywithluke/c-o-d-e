478b2ce355511fcd7605aaaf647f09b13be2330a
boywithluke <106343235+boywithluke@users.noreply.github.com>
2023-03-04 08:11:32 -0600

    Updated script section
    bug fixes

--- main.lua
+++ main.lua
@@ -1,28 +1,35 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Secret Script", "Ocean")
+local Window = Library.CreateLib("Secret Script", "Synapse")
 
--- MAIN
-local Main = Window:NewTab("Main")
-local MainSection = Main:NewSection("Main")
+-- Script
+local Script = Window:NewTab("Script")
+local ScriptSection = Script:NewSection("Script")
 
 
-MainSection:NewButton("Open Infinite Yield", "Opens Infinite Yield", function()
+ScriptSection:NewButton("Open Infinite Yield", "Opens Infinite Yield", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+
+ScriptSection:NewButton("Open ButterHub (LT2 SCRIPT)", "Opens butterhub", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Butterisgood/butter-hub/Script/Butterhub.txt'))()
+end)
+
+ScriptSection:NewButton("Open Dazed (LT2 SCRIPT)", "Opens Dazed (Made By Cazed)", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/666TranZit666/rewrite/main/Dazed%20X'))()
 end)
 
 -- LOCAL PLAYER
 local Player = Window:NewTab("Player")
 local PlayerSection = Player:NewSection("Player")
 
-PlayerSection:NewSlider("Walkspeed", "SPEED!", 500, 16, function(s) -- 500 (MaxValue) | 16 (MinValue)
+PlayerSection:NewSlider("Walkspeed", "SPEED!", 1000, 16, function(s) -- 1000 (MaxValue) | 16 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
 -- JUMP POWER
-PlayerSection:NewSlider("JumpPower", "JUMP HIGH!", 350, 50, function(s) -- 500 (MaxValue) | 16 (MinValue)
+PlayerSection:NewSlider("JumpPower", "JUMP HIGH!", 1000, 50, function(s) -- 500 (MaxValue) | 16 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
 end)
-
 
 -- OTHER
 local Other = Window:NewTab("Other")
@@ -30,4 +37,19 @@ local OtherSection = Other:NewSection("Other")
 
 OtherSection:NewButton("Chat Spoofer", "Speak for someone", function()
     loadstring(game:HttpGet("https://pastebin.com/raw/djBfk8Li"))()
+end)
+
+OtherSection:NewButton("Credits (press F9 to see the credits)", "Gives you the credits to this gui", function()
+    print("This gui was made by Codingfishwithlua#9778 and buying selling ebay#6758 discord")
+end)
+
+OtherSection:NewButton("EARLY ACCESS USERS (press F9 to see E.A. users)", "Gives you the people that have early access to the gui", function()
+    print("Codingfishwithlua#9778 and buying selling ebay#6758 have early access to the gui")
+end)
+-- TROLL --
+local Troll = Window:NewTab("Troll")
+local TrollSection = Troll:NewSection("Troll")
+
+TrollSection:NewButton("not telling you", "nope", function()
+    game.Players.LocalPlayer:Kick("Hey there dipshit, get fucking banned, join my discord server to appeal dumbass: https://discord.gg/FsQ973Vf93 ")
 end)
